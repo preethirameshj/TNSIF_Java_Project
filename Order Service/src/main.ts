9c6ec47634be49d77ebd489bf3b597505793d4b3
preethirameshj <preethirameshj@gmail.com>
2026-01-11 11:26:48 +0530

    Delete Order Service directory

--- Order Service/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { OrderDetailsModule } from './app/order.service.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(OrderDetailsModule)
-  .catch((err) => console.error(err));
